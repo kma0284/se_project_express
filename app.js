ad5882cdb5ca7984c9984799f7f411aeaa1f573f
Katelynn Anderson <kma0284@gmail.com>
2026-07-23 14:01:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const routes = require("./routes");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
     _id: "5d8b8592978f8bd833ca8133",
@@ -14,10 +18,6 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use(cors());
-
-app.use(express.json());
-
 app.use(routes);
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
