c185f753f22991142a751c6a5728402ad0b26443
Katelynn Anderson <kma0284@gmail.com>
2026-07-23 14:00:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const routes = require("./routes");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
 
+  next();
+});
 app.use(cors());
 
 app.use(express.json());
