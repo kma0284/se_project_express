742c25031612a10bfe9b5ac182fc419f111e877c
Katelynn Anderson <kma0284@gmail.com>
2026-07-26 07:59:24 -0400

    adding user context

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,13 @@ const clothingItemsRouter = require("./clothingItems");
 
 const { login, createUser } = require("../controllers/users");
 
-const { NOT_FOUND } = require("../utils/errors");
-
-// Public routes
 router.post("/signin", login);
 router.post("/signup", createUser);
 
 router.use("/items", clothingItemsRouter);
 
-// Everything below requires authentication
 router.use(auth);
 
 router.use("/users", usersRouter);
-
-router.use((req, res) =>
-  res.status(NOT_FOUND).send({
-    message: "Requested resource not found",
-  })
-);
 
 module.exports = router;
--- utils/errors.js
+++ utils/errors.js
@@ -1,3 +1,4 @@
+// status code constants
 module.exports = {
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
