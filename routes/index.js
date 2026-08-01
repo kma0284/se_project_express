fee4bb8429c6536bc9cf10fa63e95fa60d702dbf
Katelynn Anderson <kma0284@gmail.com>
2026-08-01 18:47:59 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const auth = require("../middlewares/auth");
+// const auth = require("../middlewares/auth");
 
 const {
   validateLoginBody,
@@ -18,7 +18,7 @@ router.post("/signup", validateUserBody, createUser);
 
 router.use("/items", clothingItemsRouter);
 
-router.use(auth);
+// router.use(auth);
 
 router.use("/users", usersRouter);
 
